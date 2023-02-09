cfad82bfc4b0cb2e13bb274ca4d9ff342e8c5a1a
Sam Crowley <samxcrowley@gmail.com>
2023-02-09 17:59:41 +0100

    Added react-router-dom, set up base of App and HomePage

---
+++ flashcards-frontend-react/src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+
+    return(
+        <div className="bg-slate-50 h-screen">
+            
+        </div>
+    );
+
+}
+
+const HomePage = () => {
+    return <Home />;
+};
+
+export default HomePage;
