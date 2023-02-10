977b01f29951540ba1b49c0beb19a10c43098d41
Sam Crowley <samxcrowley@gmail.com>
2023-02-10 13:12:32 +0100

    Added basic template of functionality and design to card

--- flashcards-frontend-react/src/pages/CardPage.js
+++ flashcards-frontend-react/src/pages/CardPage.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Card() {
-    
+
+    const [flipped, setFlipped] = useState(false);
+
+    let frontText = "Tilgjengelig";
+    let backText = "Available";
+
     return(
-        <div className="rounded bg-slate-500 h-64">
-            Card
+        <div className="grid grid-rows content-center">
+            <div className="rounded mx-3 my-3 shadow-sm bg-slate-500 h-64 grid grid-rows content-center gap-10">
+
+                <div className="text-white text-2xl">{frontText}</div>
+
+                <div className="text-gray-100 text-2xl">{flipped ? backText : " "}</div>
+
+            </div>
+            {!flipped &&
+                <button className="rounded h-16 bg-slate-700 text-white text-xl" onClick={() => setFlipped(true)}>
+                    Flip Card
+                </button>
+            }
+            {flipped &&
+                <div className="w-24 flex flex-row">
+                    <button>
+                        1
+                    </button>
+                    <button>
+                        2
+                    </button>
+                    <button>
+                        3
+                    </button>
+                    <button>
+                        4
+                    </button>
+                </div>
+            }
         </div>
     );
 
