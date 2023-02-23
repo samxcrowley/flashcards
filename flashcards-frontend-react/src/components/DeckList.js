ef1faa275e9bc5fc71e4ccb0543b4effa1011934
Sam Crowley <samxcrowley@gmail.com>
2023-02-23 21:31:04 +0100

    Minor colour changes of decks

--- flashcards-frontend-react/src/components/DeckList.js
+++ flashcards-frontend-react/src/components/DeckList.js
@@ -25,15 +25,15 @@ export default class DeckList extends React.Component {
                 <div className="flex flex-wrap justify-center">
                     {this.state.decks.map((value) => {
                         return(
-                            <div className="bg-slate-200 px-5 pb-4 pt-5 rounded mx-3 my-3 shadow-md w-72 text-left">
-                                <p className="text-black text-xl font-semibold mb-5">{value.name}</p>
-                                <p className="text-black mb-5">Number of cards: {value.numCards}</p>
+                            <div className="bg-slate-300 px-5 pb-4 pt-5 rounded mx-3 my-3 shadow-md w-72 text-left text-black">
+                                <p className="text-xl font-semibold mb-5">{value.name}</p>
+                                <p className="mb-5">Number of cards: {value.numCards}</p>
                                 <div className="flex items-center justify-between">
                                     <Link to={`/editDeck/${value.id}`}>
-                                        <button className="text-right font-semibold bg-slate-600 px-5 mt-3 h-8 text-white rounded">Edit</button>
+                                        <button className="text-right font-semibold bg-slate-500 px-5 mt-3 h-8 text-white rounded">Edit</button>
                                     </Link>
                                     <Link to={`/study/${value.id}`}>
-                                        <button className="text-right font-semibold bg-slate-600 px-5 mt-3 h-8 text-white rounded">Study</button>
+                                        <button className="text-right font-semibold bg-slate-500 px-5 mt-3 h-8 text-white rounded">Study</button>
                                     </Link>
                                 </div>
                             </div>
