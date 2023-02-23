57de5f1ceea64794d1a59f2da3a8ed4c5b394158
Sam Crowley <samxcrowley@gmail.com>
2023-02-23 11:14:37 +0100

    Set up deck editing page

--- flashcards-frontend-react/src/pages/EditDeckPage.js
+++ flashcards-frontend-react/src/pages/EditDeckPage.js
@@ -5,44 +5,61 @@ import axios from "axios";
 function EditDeckPage() {
 
     const { deckId } = useParams();
-    const { deckName } = "";
+    const [ deck, setDeck ] = useState(null);
     const [ cards, setCards ] = useState([]);
 
     useEffect(() => {
+
+        axios.get("/api/decks/" + deckId)
+            .then(response => {
+                console.log(response);
+                setDeck(response.data);
+            });
+
         axios.get("/api/decks/" + deckId + "/cards")
             .then(response => {
                 setCards(response.data);
             });
-    }, []);
+
+    });
 
     return(
-        <div className="py-24">
-            <div className="mb-5">{deckName}</div>
-            <table className="m-auto">
-                <thead>
-                    <tr>
-                        <th>Front text</th>
-                        <th>Back text</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cards.map((value) => {
-                        return(
-                            <tr>
-                                <td className="px-5 py-2">{value.frontText}</td>
-                                <td className="px-5 py-2">{value.backText}</td>
-                                <td className="px-3 py-2">
-                                    <button className="font-semibold bg-myorange-600 rounded text-white px-2 py-1">Edit</button>
-                                </td>
-                                <td className="px-1 py-2">
-                                    <button className="font-semibold bg-myorange-600 rounded text-white px-2 py-1">Delete</button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+        <div className="my-24 pt-4 w-1/2 m-auto bg-slate-100 rounded shadow">
+            {deck &&
+                <div className="mb-5 text-2xl font-bold text-slate-800">{deck.name}</div>
+            }
+            <div className="flex flex-row justify-center mb-5">
+                <button className="px-3 mx-3 my-1 rounded bg-red-400 text-white font-semibold">Delete deck</button>
+                <button className="px-3 mx-3 my-1 rounded bg-slate-600 text-white font-semibold">Edit deck name</button>
+                <button className="px-3 mx-3 my-1 rounded bg-slate-600 text-white font-semibold">Add card</button>
+            </div>
+            <div className="bg-slate-200 border-10 py-5">
+                <table className="m-auto">
+                    <thead>
+                        <tr>
+                            <th>Front text</th>
+                            <th>Back text</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cards.map((value) => {
+                            return(
+                                <tr className="even:bg-slate-300 text-left">
+                                    <td className="px-5 py-2">{value.frontText}</td>
+                                    <td className="px-5 py-2">{value.backText}</td>
+                                    <td className="px-3 py-2">
+                                        <button className="font-semibold bg-slate-400 rounded text-white px-2">Edit</button>
+                                    </td>
+                                    <td className="px-1 py-2">
+                                        <button className="font-semibold bg-slate-400 rounded text-white px-2">Delete</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 
