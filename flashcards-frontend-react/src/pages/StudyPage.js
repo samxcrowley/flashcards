1b456a35900f5f4249addb8a6e0fdf91dfcf86fa
Sam Crowley <samxcrowley@gmail.com>
2023-02-23 21:53:19 +0100

    Added functionality for StudyPage to retrieve list of cards from appropriate deck and present them in order

--- flashcards-frontend-react/src/pages/StudyPage.js
+++ flashcards-frontend-react/src/pages/StudyPage.js
@@ -1,79 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
+import axios from "axios";
 
-export default class StudyPage extends React.Component {
+function StudyPage() {
 
-    // need reference to current card (card ID)
-    // so that can send back data about user's response
+    const [ cards, setCards ] = useState([{"frontText": "Loading cards..."}]);
+    const { deckId } = useParams();
+    const [ currentCardIndex, setCurrentCardIndex ] = useState(0);
+    const [ flipped, setFlipped ] = useState(false);
+    const [ deckDone, setDeckDone ] = useState(false);
 
-    state = {
-        cards: [{frontText: "Regjering", backText: "Government"},
-                {frontText: "Gård", backText: "Farm"},
-                {frontText: "Slott", backText: "Palace"}],
-        currentCardIndex: 0,
-        cardFlipped: false,
-        deckDone: false
+    useEffect(() => {
+
+        axios.get("/api/decks/" + deckId + "/cards")
+            .then(response => {
+                setCards(response.data);
+            });
+
+    }, []);
+
+    const flipCard = () => {
+        setFlipped(true);
     }
 
-    flipCard() {
-        this.setState(state => ({cardFlipped: true}));
-    }
+    const nextCard = () => {
 
-    nextCard() {
-        let oldIndex = this.state.currentCardIndex;
-        if (oldIndex < this.state.cards.length - 1) {
-            this.setState(state => ({currentCardIndex: oldIndex + 1, cardFlipped: false}));
+        let oldIndex = currentCardIndex;
+
+        if (oldIndex < cards.length - 1) {
+            setCurrentCardIndex(oldIndex + 1);
+            setFlipped(false);
         } else {
-            this.setState(state => ({deckDone: true}));
+            setDeckDone(true);
         }
+
     }
 
-    render() {
+    return(
+        <div className="bg-white h-screen">
 
-        let currentCard = this.state.cards[this.state.currentCardIndex];
+            {deckDone &&
+                <div className="py-36 grid grid-rows content-center justify-center">
+                    <div className="text-2xl py-16">You have completed your reviews of this deck for today!</div>
+                    <Link to="/">
+                        <button className="rounded px-6 py-1 h-12 text-black text-xl font-bold">Return Home</button>
+                    </Link>
+                </div>
+            }
 
-        return(
-            <div className="bg-white h-screen">
-
-                {this.state.deckDone &&
-                    <div className="py-36 grid grid-rows content-center justify-center">
-                        <div className="text-2xl py-16">You have completed your reviews of this deck for today!</div>
-                        <Link to="/">
-                            <button className="rounded px-6 py-1 h-12 text-black text-xl font-bold">Return Home</button>
-                        </Link>
-                    </div>
-                }
-
-                {!this.state.deckDone &&
-                    <div className="py-36 grid grid-rows content-center justify-center">
-                        <Card frontText={currentCard.frontText} backText={currentCard.backText} flipped={this.state.cardFlipped} />
-                        {!this.state.cardFlipped &&
-                            <button className="w-96 text-black text-xl" onClick={() => this.flipCard()}>
-                                Flip Card
+            {!deckDone && cards &&
+                <div className="py-36 grid grid-rows content-center justify-center">
+                    <Card frontText={cards[currentCardIndex].frontText} backText={cards[currentCardIndex].backText} flipped={flipped} />
+                    {!flipped &&
+                        <button className="w-96 text-black text-xl" onClick={() => flipCard()}>
+                            Flip Card
+                        </button>
+                    }
+                    {flipped &&
+                        <div className="w-96 flex flex-row justify-between">
+                            <button className="rounded px-2 py-1 bg-red-500 text-white font-semibold text-lg" onClick={() => nextCard()}>
+                                Again (1)
                             </button>
-                        }
-                        {this.state.cardFlipped &&
-                            <div className="w-96 flex flex-row justify-between">
-                                <button className="rounded px-2 py-1 bg-red-500 text-white font-semibold text-lg" onClick={() => this.nextCard()}>
-                                    Again (1)
-                                </button>
-                                <button className="rounded px-2 py-1 bg-orange-500 text-white font-semibold text-lg" onClick={() => this.nextCard()}>
-                                    Hard (2)
-                                </button>
-                                <button className="rounded px-2 py-1 bg-yellow-500 text-white font-semibold text-lg" onClick={() => this.nextCard()}>
-                                    Good (3)
-                                </button>
-                                <button className="rounded px-2 py-1 bg-green-500 text-white font-semibold text-lg" onClick={() => this.nextCard()}>
-                                    Easy (4)
-                                </button>
-                            </div>
-                        }
-                    </div>
-                }
-            </div>
-        );
+                            <button className="rounded px-2 py-1 bg-orange-500 text-white font-semibold text-lg" onClick={() => nextCard()}>
+                                Hard (2)
+                            </button>
+                            <button className="rounded px-2 py-1 bg-yellow-500 text-white font-semibold text-lg" onClick={() => nextCard()}>
+                                Good (3)
+                            </button>
+                            <button className="rounded px-2 py-1 bg-green-500 text-white font-semibold text-lg" onClick={() => nextCard()}>
+                                Easy (4)
+                            </button>
+                        </div>
+                    }
+                </div>
+            }
+        </div>
+    );
 
-    }
+}
 
-};
+export default StudyPage;
